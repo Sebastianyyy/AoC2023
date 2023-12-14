a24f14094cc293ff314b43e79078d90435631d42
Sebastian Musial (Sebastianyyy) <64799476+Sebastianyyy@users.noreply.github.com>
2023-12-14 14:25:43 +0100

    day13

---
+++ day13/day13-part2.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def rows_mirror(array):
+    for i in range(1,array.shape[0]//2+1):
+        first=array[:i,:]
+        second=array[i:2*i,:]
+        if np.sum((first==second[::-1,:]).astype(int))==((first.shape[0]*first.shape[1])-1):
+            return i
+    for i in range(array.shape[0]//2+1,array.shape[0]):
+        j=i-(array.shape[0]-i)
+        first=array[i:,:]
+        second=array[j:i,:]
+        if np.sum((first==second[::-1,:]).astype(int))==((first.shape[0]*first.shape[1])-1):
+            return i
+    return 0
+
+def cols_mirror(array):
+    for i in range(1,array.shape[1]//2+1):
+        first=array[:,:i]
+        second=array[:,i:2*i]
+        if np.sum((first==second[:,::-1]).astype(int))==((first.shape[0]*first.shape[1])-1):
+
+            return i
+    for i in range(array.shape[1]//2+1,array.shape[1]):
+        j=i-(array.shape[1]-i)
+        first=array[:,i:]
+        second=array[:,j:i]
+        if np.sum((first==second[:,::-1]).astype(int))==((first.shape[0]*first.shape[1])-1):
+
+            return i
+    return 0
+
+
+if __name__=='__main__':
+    with open(file='input.txt') as f:
+        lines=f.readlines()
+    t=[]
+    s=0
+    for i in (lines):
+        if i!='\n':
+            t.append(list(i[:-1]))
+        else:
+            array=(np.vstack([k for k in t]))
+            t=[]
+            s+=cols_mirror(array)
+            s+=rows_mirror(array)*100
+    print(s)
